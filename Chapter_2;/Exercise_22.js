3571e42f743678e76c224c68dcd41e3149f6fedf
Mohammad <jamim1382@gmail.com>
2025-09-24 12:24:36 +0430

    add-exercuse-22-and-23

--- Chapter_2;/Exercise_22.js
+++ Chapter_2;/Exercise_22.js
@@ -1,6 +1,12 @@
 // 2.22 (Financial application: monetary units)
 // Rewrite the “compute change” program in JavaScript to avoid accuracy loss when converting floating-point numbers to integers.
-
 // 2.22 (کاربرد مالی: واحدهای پولی)
 // یک برنامه JavaScript بنویسید که از کاربر مبلغ را به صورت عدد صحیح (با دو رقم آخر برای سنت‌ها) بگیرد و آن را به دلار و سنت تبدیل کرده و نمایش دهد.
 
+var user = Number(prompt('enter a number'))
+
+var change = user / 100
+
+var take = change % 100
+
+console.log(take)
--- Chapter_2;/Exercise_23.js
+++ Chapter_2;/Exercise_23.js
@@ -1,3 +1,12 @@
 // 2.23 (هزینه رانندگی)
 // یک برنامه JavaScript بنویسید که از کاربر مسافت رانندگی، مصرف سوخت خودرو (مایل بر گالن) و قیمت هر گالن را بگیرد، سپس هزینه‌ی سفر را محاسبه و نمایش دهد.
 
+var user =Number(prompt('enter a number'))
+
+var gallan =Number(prompt('enter a number'))
+
+var division = user / gallan
+
+var price = division * 3.55
+
+console.log(price)
