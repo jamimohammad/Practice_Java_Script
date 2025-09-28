850429e29a87e1c9a174f96a9184b5e3d3f541fc
Mohammad <jamim1382@gmail.com>
2025-09-28 10:12:01 +0430

    add-exercise1-and-3

--- Chapter_3;/Exercise_1.js
+++ Chapter_3;/Exercise_1.js
@@ -1,2 +1,21 @@
 // 3.1 (Algebra: حل معادله درجه دوم)
 // برنامه‌ای در جاوااسکریپت بنویسید که از کاربر سه مقدار a, b, c بگیرد و ریشه‌های معادله درجه دوم ax² + bx + c = 0 را محاسبه و نمایش دهد. اگر دترمینان مثبت باشد دو ریشه واقعی نمایش داده شود، اگر صفر باشد یک ریشه، و اگر منفی باشد پیغام بدهد "The equation has no real roots".
+
+var usera = Number(prompt("Enter a:"));
+var userb = Number(prompt("Enter b:"));
+var userc = Number(prompt("Enter c:"));
+
+var all = userb**2 - 4*usera*userc;
+
+  var r1 = (-userb + Math.sqrt(all)) / (2*usera);
+  var r2 = (-userb - Math.sqrt(all)) / (2*usera);
+   var r = -userb / (2*usera);
+
+if (all > 0) {
+  console.log("the equation has two roots: " + r1 + " and " + r2);
+} else if (all === 0) {
+  console.log("the equation has one root: " + r);
+} else {
+  console.log('the equation has no root');
+}
+
--- Chapter_3;/Exercise_3.js
+++ Chapter_3;/Exercise_3.js
@@ -1,2 +1,19 @@
 // 3.3 (Algebra: حل دستگاه معادلات خطی 2×2)
 // برنامه‌ای در جاوااسکریپت بنویسید که مقادیر a, b, c, d, e, f را از کاربر بگیرد و دستگاه معادلات خطی 2×2 را با قانون کرامر حل کند. اگر ad - bc = 0 بود پیام بدهد که "The equation has no solution".
+var usera = Number(prompt('enter a number'))
+var userb = Number(prompt('enter a number'))
+var userc = Number(prompt('enter a number'))
+var userd = Number(prompt('enter a number'))
+var usere = Number(prompt('enter a number'))
+var userf = Number(prompt('enter a number'))
+
+var plus = (usera * userd) - (userb * userc)
+
+if (plus === 0){
+    alert('the equation has no solution')
+}else  {
+    var x = (usere * userd - userb * userf) / plus;
+    var y = (usera * userf - usere * userc) / plus;
+    alert('x is ' + x + ' and y is ' + y);
+}
+
