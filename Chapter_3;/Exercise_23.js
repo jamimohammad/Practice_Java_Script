19b579f7e2dd707c78420b0293c637aa4466533c
Mohammad <jamim1382@gmail.com>
2025-10-06 09:38:26 +0430

    add exercise 24

--- Chapter_3;/Exercise_23.js
+++ Chapter_3;/Exercise_23.js
@@ -1,2 +1,20 @@
 // 3.23 (هندسه: نقطه در مستطیل؟)
 // برنامه‌ای در جاوااسکریپت بنویسید که مختصات یک نقطه را بگیرد و بررسی کند آیا داخل مستطیل با مرکز (0,0)، عرض 10 و ارتفاع 5 قرار دارد یا نه.
+var x = Number(prompt('enter a number'))
+var y = Number(prompt('enter a number'))
+var width = 10
+var height = 5
+var division = width / 2
+
+var division1 = height / 2
+
+if (x <= division && y <= division1){
+    console.log('داخل است')
+}else{
+    console.log ('بیرون است')
+}
+// if (Math.abs(x) <= division && Math.abs(y) <= division1){
+//     console.log('داخل است')
+// }else{
+//     console.log('بیرون است')
+// }
--- Chapter_3;/Exercise_24.js
+++ Chapter_3;/Exercise_24.js
@@ -1,3 +1,51 @@
 // 3.24 (بازی: انتخاب یک کارت) برنامه‌ای در جاوااسکریپت بنویسید که یک کارت تصادفی از 52 کارت انتخاب کند و نام آن (عدد/عکس + خال) را نمایش دهد.
+var take = Math.floor(Math.random() * 52);
 
-var take = Math.floor(Math.random() * 52)
+var all = Math.floor(take / 13);   
+var all1 = take % 13;               
+
+var one=''
+if (all == 0) {
+    one = "Clubs";
+} else if (all == 1) {
+    one = "Diamonds";
+} else if (all == 2) {
+    one = "Hearts";
+} else if (all == 3) {
+    one = "Spades";
+} else {
+    one = "bad";
+}
+
+var rank=''
+if (all1 == 0) {
+    rank = "Ace";
+} else if (all1 == 1) {
+    rank = "2";
+} else if (all1 == 2) {
+    rank = "3";
+} else if (all1 == 3) {
+    rank = "4";
+} else if (all1 == 4) {
+    rank = "5";
+} else if (all1 == 5) {
+    rank = "6";
+} else if (all1 == 6) {
+    rank = "7";
+} else if (all1 == 7) {
+    rank = "8";
+} else if (all1 == 8) {
+    rank = "9";
+} else if (all1 == 9) {
+    rank = "10";
+} else if (all1 == 10) {
+    rank = "Jack";
+} else if (all1 == 11) {
+    rank = "Queen";
+} else if (all1 == 12) {
+    rank = "King";
+} else {
+    rank = "bad";
+}
+alert("The card you picked is " + rank + " of " + one);
+console.log("Card number:", take, rank + " of " + one);
