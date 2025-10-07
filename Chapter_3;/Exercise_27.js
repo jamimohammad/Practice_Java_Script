d99bf41376c820b582803b5224d3e7a2caeaacc2
Mohammad <jamim1382@gmail.com>
2025-10-07 11:02:39 +0430

    add exercise 27

--- Chapter_3;/Exercise_27.js
+++ Chapter_3;/Exercise_27.js
@@ -1,1 +1,9 @@
 //3.27 (هندسه: نقطه در مثلث راست‌گوشه؟) برنامه‌ای در جاوااسکریپت بنویسید که مختصات یک نقطه را بگیرد و بررسی کند آیا در مثلثی که رأس‌هایش (0,0)، (200,0) و (0,100) هستند قرار دارد یا نه
+var x = Number(prompt('enter a number'))
+var y = Number(prompt('enter a number'))
+
+if (x >= 0 && y >= 0 && y <= (-0.5 * x + 100)) {
+    console.log("The point is in the triangle");
+} else {
+    console.log("The point is not in the triangle");
+}
