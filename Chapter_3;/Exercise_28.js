8eaad294e0f2cdaebc4f3eecc59a659d929366f5
Mohammad <jamim1382@gmail.com>
2025-10-08 10:07:16 +0430

    add exercise 30

--- Chapter_3;/Exercise_28.js
+++ Chapter_3;/Exercise_28.js
@@ -1,11 +1,23 @@
 // 3.28 (هندسه: دو مستطیل)
 // برنامه‌ای در جاوااسکریپت بنویسید که مختصات مرکز، عرض و ارتفاع دو مستطیل را بگیرد و بررسی کند آیا مستطیل دوم داخل اولی است، با آن همپوشانی دارد یا هیچ‌کدام.
-var x = Number(prompt('enter a number'))
-var y = Number(prompt('enter a number'))
+var x = Number(prompt('x enter the first rectangle'))
+var y = Number(prompt('y enter the first rectangle'))
+var weight = Number(prompt('w enter the first rectangle'))
+var height = Number(prompt('h enter the first rectangle'))
 
-var plus = -0.5 * x + 100
-if (plus > y){
-    alert('نقطه داخل مثلث است')
-}else{
-    alert('نقطه داخل مثلث نیست')
-}
+var x1 = Number(prompt('x1 enter the first rectangle'))
+var y1 = Number(prompt('y1 enter the first rectangle'))
+var weight1 = Number(prompt('w1 enter the first rectangle'))
+var height1 = Number(prompt('h1 enter the first rectangle'))
+
+var left = x - weight / 2
+var right = x + weight / 2
+var bottom = y - height / 2
+var top0 = y + height / 2
+
+
+var left1 = x1 - weight1 / 2
+var right1 = x1 + weight1 / 2
+var bottom1 = y1 - height1 / 2
+var top1 = y1 + height1 / 2
+
--- Chapter_3;/Exercise_30.js
+++ Chapter_3;/Exercise_30.js
@@ -1,1 +1,21 @@
-// 3.30 (زمان فعلی) برنامه‌ای در جاوااسکریپت بنویسید که زمان فعلی را با توجه به اختلاف ساعت منطقه زمانی کاربر نسبت به GMT محاسبه و با فرمت 12 ساعته نمایش دهد.
+// 3.30 (زمان فعلی) برنامه‌ای در جاوااسکریپت بنویسید که زمان فعلی را با توجه به اختلاف ساعت منطقه زمانی کاربر نسبت به GMT محاسبه و با فرمت 12 ساعته نمایش دهد.var user = Number(prompt('enter a number '))
+
+var take = Math.floor(user * 60)
+
+var milliseconds = Date.now();
+console.log(milliseconds);
+
+var seconds = Math.floor(milliseconds / 1000 );
+
+var take1 = seconds % 60;
+
+var minutes = Math.floor(seconds / 60 + take);
+
+var take2 = minutes % 60
+
+var hours = Math.floor(minutes / 60 );
+
+var take3 = hours % 24
+
+console.log('current time' + ':' +'Hours'+':'  + take3 + ':' +'minutes'+':'  + take2 + ':' + 'seconds'+':'+ take1)
+
