b8eb98e34805631e8612794cdd830ec0717b1f31
Mohammad <jamim1382@gmail.com>
2025-10-19 08:48:03 +0430

    add exercise 4

---
+++ Chapter_4;/Exercise_3.js
@@ -0,0 +1,10 @@
+// (Geometry: area of a hexagon) The area of a hexagon can be computed using the 
+// following formula (s is the length of a side):
+
+var s = Number(prompt('enter a number'))
+var multipliction = 6 * s ** 2
+
+var math1 = Math.tan(Math.PI / 6) * 4
+var area = multipliction / math1
+
+console.log(area)
