89dd2836f7fac5df2b9789b6774a5a12793d7ef0
Mohammad <jamim1382@gmail.com>
2025-10-21 10:33:13 +0430

    add exercise 9

--- Chapter_4;/Exercise_8.js
+++ Chapter_4;/Exercise_8.js
@@ -1,3 +1,5 @@
+// *4.8 (Find the character of an ASCII code) Write a program that receives an ASCII code 
+// (an integer between 0 and 127) and displays its character. Here is a sample run:
 
 var user = Number(prompt('Enter a number'))
 var user1 = String.fromCharCode(user)
---
+++ Chapter_4;/Exercise_9.js
@@ -0,0 +1,7 @@
+// 4.9 (Find the Unicode of a character) Write a program that receives a character and 
+// displays its Unicode. Here is a sample run:
+
+var user =prompt('Enter a number')
+var user1 =user.charCodeAt(0)
+
+console.log(user1)
