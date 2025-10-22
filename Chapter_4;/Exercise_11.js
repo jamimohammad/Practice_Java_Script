e6b9e48991a49314ce2836cfdc613a8b9b00a78a
Mohammad <jamim1382@gmail.com>
2025-10-22 10:10:11 +0430

    add exercise 11

---
+++ Chapter_4;/Exercise_11.js
@@ -0,0 +1,25 @@
+var user = Number(prompt('enter a number bettwen 0 , 15'))
+
+if (user >= 0 && user <= 9){
+    alert(user)
+}else if(user == 10){
+alert('A')
+}
+else if(user == 11){
+alert('B')
+}
+else if(user == 12){
+alert('C')
+}
+else if(user == 13){
+alert('D')
+}
+else if(user == 14){
+alert('E')
+}
+else if(user == 15){
+alert('F')
+}
+else{
+    alert('bad')
+}
