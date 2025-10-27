51ef294b1af8d6c3c24e21f851bd4dc5a51114c6
Mohammad <jamim1382@gmail.com>
2025-10-27 15:03:26 +0430

    add exercise 16

---
+++ Chapter_4;/Exercise_16.js
@@ -0,0 +1,11 @@
+// (Random character) Write a program that displays a random uppercase letter 
+// using the Math.random() method.
+
+function all(){
+var random = Math.floor(Math.random() * 26); 
+var letter = random + 65; 
+var random1 = String.fromCharCode(letter); 
+alert(random1); 
+}
+all()
+
