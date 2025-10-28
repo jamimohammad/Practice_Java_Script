a9b90ba7a0cbc822a446a0a98dedf32729e4ca91
Mohammad <jamim1382@gmail.com>
2025-10-28 10:31:19 +0430

    add exercise 17

--- Chapter_2;/Exercise_02.js
+++ Chapter_2;/Exercise_02.js
@@ -1,8 +1,8 @@
 //4.Declare a variable radius = 6. Calculate and print the area and circumference of the circle.
 let radius = 6;
-let length = 12; // طول استوانه
+let length = 12; 
 let area = 3.14 * (radius ** 2);
-let volume = area * length; // حجم = مساحت × طول
+let volume = area * length; 
 console.log(area);
 console.log(volume);
 
