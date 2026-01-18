a135e4011d151af1ca601f2a36a4bcd86b9e8045
Mohammad <jamim1382@gmail.com>
2026-01-18 10:15:49 +0430

    questiones 17 and 18 solved

---
+++ chapter-6-question-eleven-to-twenty/Exercise_18.js
@@ -0,0 +1,33 @@
+// **6.18 (Check password) Some websites impose certain rules for passwords. Write a 
+// method that checks whether a string is a valid password. Suppose the password 
+// rules are as follows:
+//  ■ A password must have at least eight characters.
+//  ■ A password consists of only letters and digits.
+//  ■ A password must contain at least two digits.
+// Write a program that prompts the user to enter a password and displays Valid
+// Password if the rules are followed or Invalid Password otherwise.
+
+let password = prompt('please enter your password');
+
+if (password.length < 8) {
+  console.log("Invalid Password");
+} 
+
+else if (!/^[A-Za-z0-9]+$/.test(password)) {
+  console.log("Invalid Password");
+} 
+
+else {
+  let digitCount = 0;
+  for (let i = 0; i < password.length; i++) {
+    if (password[i] >= '0' && password[i] <= '9') {
+      digitCount++;
+    }
+  }
+
+  if (digitCount >= 2) {
+    console.log("Valid Password");
+  } else {
+    console.log("Invalid Password");
+  }
+}
