a796c43950b58a68593ce37117085b2e16667bd6
Mohammad <jamim1382@gmail.com>
2026-05-05 10:32:43 +0430

    add some changes in question 34

--- Chapter_5/Chapter_5_From questions thirty-two to forty/Exercise_34.js
+++ Chapter_5/Chapter_5_From questions thirty-two to forty/Exercise_34.js
@@ -1,44 +1,21 @@
-// ***5.34 (Game: scissor, rock, paper) Programming Exercise  3.17 gives a program that 
-// plays the scissor-rock-paper game. Revise the program to let the user continu
-// ously play until either the user or the computer wins more than two times than its 
-// opponent.
+// // ***5.34 (Game: scissor, rock, paper) Programming Exercise  3.17 gives a program that 
+// // plays the scissor-rock-paper game. Revise the program to let the user continu
+// // ously play until either the user or the computer wins more than two times than its 
+// // opponent.
 
-let userinput = 0
-let computerinput = 0
+for (;;) {
+    let user = Number(prompt('Enter: scissor(0) rock(1) paper(2)'));
+    let computer = Math.floor(Math.random() * 3);
 
-for (; ;) {
+    alert('Computer: ' + computer);
 
-    let userinput = Number(prompt('Please Enter a Number'+'       '+ 'scissor(0)' + '     rock(1)' + '    paper(2)'))
+    let result = (user - computer + 3) % 3;
 
-    let computerinput = Math.floor(Math.random() * 3)
-    alert('computerinput' + "      " + computerinput)
-
-    if (computerinput === userinput) {
-        alert('Equal')
-    } else if (computerinput === 0 && userinput === 1) {
-        userinput++
-        alert( 'The user won')
-    } else if (computerinput === 0 && userinput === 2) {
-        computerinput++
-        alert('The user failed')
-    } else if (computerinput === 1 && userinput === 2) {
-        userinput++
-        alert( 'The user won')
-    } else if (computerinput === 2 && userinput === 0) {
-        userinput++
-        alert( 'The user won')
-    } else if (computerinput === 2 && userinput === 1) {
-        computerinput++
-        alert('the user failed')
-    } else if (computerinput === 1 && userinput === 0) {
-        computerinput++
-        alert('the user failed again')
+    if (result === 0) {
+        alert('Equal');
+    } else if (result === 1) {
+        alert('User won');
     } else {
-        alert('bad')
+        alert('User lost');
     }
-
-    if (userinput - computerinput > 2 || computerinput - userinput > 2) {
-        break;
-    }
-
 }
